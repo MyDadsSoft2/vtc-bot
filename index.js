80422336d04cff64815bb70a96b878e0ec67281d
MyDadsSoft2 <tsucrashed@gmail.com>
2026-06-30 20:38:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const AUTHORIZED_ROLE_IDS = [
 // ====== DON'T TYPE CHANNEL CONFIG ======
 const DONT_TYPE_CHANNEL_ID = "1521598265215160360";
 const DONT_TYPE_NOTICE =
-  "🚫 **DON'T TYPE CHANNEL**\n\n" +
-  "This channel is here as an anti-scammer trap. Anyone who posts messages, images, links, or attachments here will be automatically banned.\n\n" +
-  "Staff: do not test this unless you know you are exempt.";
+  "**TYPE MODS**\n\n" +
+  "Type mods and you will get a dm from the bot with mods.\n\n" +
+  "Warning Some Are paid.";
 
 // ====== DRIVER COUNT CONFIG ======
 const GUILD_ID = "1387897307361575215";
